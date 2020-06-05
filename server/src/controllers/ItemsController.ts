21f5c62dd05199c2b597eacb12b25ea522e72750
yuziem14 <yuri.fnuens@gmail.com>
2020-06-05 03:25:09 -0300

    refactor: update image url to network IP

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -8,7 +8,7 @@ class ItemsController {
     const serializedItems = items.map(item => ({
       id: item.id,
       title: item.title,
-      image_url: `http://127.0.0.1:3333/uploads/${item.image}`,
+      image_url: `http://192.168.1.188:3333/uploads/${item.image}`,
     }));
 
     return response.json(serializedItems);
