b84024a7af54d7c3fc8a025032e5b0dc5a427ce7
yuziem14 <yuri.fnunes@gmail.com>
2020-06-05 05:52:23 -0300

    feat: add app routes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,26 @@
+const path = require('path');
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const server = express();
 
 server.use(express.static('public'));
 
+nunjucks.configure(path.resolve(__dirname, 'views'), {
+  express: server,
+  noCache: true,
+});
+
 server.get('/', (request, response) => {
-  return response.sendFile(`${__dirname}/views/index.html`);
+  return response.render('index.html');
+});
+
+server.get('/create-point', (request, response) => {
+  return response.render('create-point.html');
+});
+
+server.get('/search', (request, response) => {
+  return response.render('search-results.html');
 });
 
 const PORT = 3000;
