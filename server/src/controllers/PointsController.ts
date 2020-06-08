a176c41563f9c3a8e82f48044b9f4728e826d364
yuziem14 <yuri.fnunes@gmail.com>
2020-06-07 22:55:45 -0300

    bugfix: items filter not working properly solved

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -10,10 +10,10 @@ class PointsController {
       .map(item => Number(item.trim()));
 
     const points = await knex('points')
-      .join('point_items', 'point_id', '=', 'point_items.point_id')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
       .whereIn('point_items.item_id', parsedItems)
-      .where('city', String(city))
-      .where('uf', String(uf))
+      .where('city', 'LIKE', String(city))
+      .where('uf', 'LIKE', String(uf))
       .distinct()
       .select('points.*');
 
